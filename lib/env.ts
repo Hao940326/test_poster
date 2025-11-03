4cb841bb5610fb75c019b600b4cc5335985f3502
hao940326 <a0903751284@gmail.com>
2025-11-03 20:07:34 +0800

    update: change redirect to /edit on production

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,4 @@
 export const ORIGIN = process.env.NEXT_PUBLIC_ORIGIN!;
-export const BASE = process.env.NEXT_PUBLIC_BASE_PREFIX!; // "/studio" 或 "/edit"
+export const BASE = process.env.NEXT_PUBLIC_BASE_PREFIX!;
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-export const SUPABASE_ANON = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
